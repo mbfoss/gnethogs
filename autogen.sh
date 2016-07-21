0705034344aec834accf4cf3d7128b861981668b
Mohamed Boussaffa <mbfoss@fastmail.com>
2016-07-21 22:29:04 +0800

    fixing configure.ac

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,12 @@
-autoreconf --install || exit 1
+### AUTOTOOLS #######################################################
+# Runs autoconf, autoheader, aclocal, automake, autopoint, libtoolize
+echo "Regenerating autotools files"
+autoreconf --install --force --symlink || exit 1
+
+### INTLTOOL ########################################################
+# Run after autopoint or glib-gettextize
+echo "Setting up intltool"
+intltoolize --copy --force --automake || exit 1
+
+echo "Now run ./configure, make, and make install."
 
